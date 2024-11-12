d6d70eaeeec9c6bdf0a06c570e96c6b20123bc02
lgc1150 <lgc1150@naver.com>
2024-11-12 20:09:14 +0900

    2024/11/12

--- member_post/src/main/java/service/MemberServiceImpl.java
+++ member_post/src/main/java/service/MemberServiceImpl.java
@@ -2,19 +2,27 @@ package service;
 
 import java.util.List;
 
-import dao.MemberDao;
+import org.apache.ibatis.session.SqlSession;
+
+import mapper.MemberMapper;
+import utils.MybatisInit;
 import vo.Member;
 
 public class MemberServiceImpl implements MemberService {
-	private MemberDao memberDao = MemberDao.getInstance();
 	@Override
 	public int register(Member member) {
-		return memberDao.insert(member);
+		try(SqlSession session = MybatisInit.getInstance().sqlSessionFactory().openSession(true)) {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+			return mapper.insert(member);
+		}
 	}
 
 	@Override
 	public Member findBy(String id) {
-		return memberDao.selectOne(id);
+		try(SqlSession session = MybatisInit.getInstance().sqlSessionFactory().openSession(true)) {
+			MemberMapper mapper = session.getMapper(MemberMapper.class);
+			return mapper.selectOne(id);
+		}
 	}
 
 	@Override
