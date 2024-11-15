9fa3c16a3eab0527399dc727b0d6718da3ed154d
lgc1150 <lgc1150@naver.com>
2024-11-15 18:22:02 +0900

    2024/11/15

--- member_post/src/main/java/servlet/reply/ReplyController.java
+++ member_post/src/main/java/servlet/reply/ReplyController.java
@@ -8,15 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import service.ReplyService;
 import service.ReplyServiceImpl;
+import vo.Reply;
 
 @WebServlet("/reply/*")
 public class ReplyController extends HttpServlet{
 
     private ReplyService service = ReplyServiceImpl.getInstance();
+    private Gson gson = new GsonBuilder().setDateFormat("yyyy/MM/dd-HH:mm:ss").create();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -25,19 +29,19 @@ public class ReplyController extends HttpServlet{
         uri = uri.replace(req.getContextPath() + "/reply/", "");
         System.out.println(uri);
         Object ret = null;
-        if(uri.startsWith("list")) {
+        if(uri.startsWith("list")) { // 목록 조회
             int tmpIdx = uri.lastIndexOf("/");
             Long pno =  0L;
             if(tmpIdx != -1) {
                 pno = Long.valueOf(uri.substring(tmpIdx+1));
             }
             ret = service.list(pno);
-        }else {
+        }else { // 단일 조회
             Long rno = Long.valueOf(uri);
             ret = service.findBy(rno);
         }
         resp.setContentType("application/json; charset=utf-8");
-        resp.getWriter().print(new ObjectMapper().writeValueAsString(ret));
+        resp.getWriter().print(gson.toJson(ret));
     }
 
     @Override
@@ -47,18 +51,47 @@ public class ReplyController extends HttpServlet{
     	req.getReader().read(chs);
     	String str = new String(chs);
     	System.out.println(str);
+    	
+//    	JsonNode node = mapper.readTree(str);
+//    	System.out.println(node);
+    	Reply reply = gson.fromJson(str, Reply.class);
+    	System.out.println(reply);
+    	
+    	service.write(reply);
+//    	service.write(gson.fromJson(req.getReader(), Reply.class));
     }
 
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPut(req, resp);
+    	char[] chs = new char[req.getContentLength()];
+    	req.getReader().read(chs);
+    	String str = new String(chs);
+    	System.out.println(str);
+    	
+//    	JsonNode node = mapper.readTree(str);
+//    	System.out.println(node);
+    	Reply reply = gson.fromJson(str, Reply.class);
+    	System.out.println(reply);
+    	
+    	service.write(reply);
     }
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doDelete(req, resp);
-    }
+    	String uri = req.getRequestURI();
+        uri = uri.replace(req.getContextPath() + "/reply/", "");
 
+        if(uri.startsWith("list")) { // 목록 조회
+            int tmpIdx = uri.lastIndexOf("/");
+            Long pno =  0L;
+            if(tmpIdx != -1) {
+                pno = Long.valueOf(uri.substring(tmpIdx+1));
+            }
+            service.removeAll(pno);
+        }
+        else { // 단일 조회
+        	Long rno = Long.valueOf(uri);
+        	service.remove(rno);
+        }
+    }
 }
--- member_post/src/main/java/vo/Attach.java
+++ member_post/src/main/java/vo/Attach.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.ognl.ObjectMethodAccessor;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+
 
 import lombok.Builder;
 import lombok.Data;
@@ -21,14 +19,5 @@ public class Attach {
 	private boolean image;
 	private Long pno;
 	
-	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException {
-		List<Attach> attachs = new ArrayList<Attach>();
-		attachs.add(Attach.builder().uuid("abcd").origin("tmp.txt").build());
-		attachs.add(Attach.builder().uuid("efgh").origin("tmp.png").build());
-		
-		System.out.println(attachs);
-		
-		String s = new ObjectMapper().writeValueAsString(attachs);
-		System.out.println(s);
-	}
+
 }
